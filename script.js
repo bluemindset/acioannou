80210c0acc4cfee0a027b5fcd8618135f730abde
Stefanos Ioannou <bluemindset@outlook.com>
2025-05-09 21:13:54 +0300

    context

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ setInterval(() => {
     currentSlide = (currentSlide + 1) % 2;
     carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
 }, 4000);
+
+
+// gtag-init.js
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-CN02KN9M2K');
